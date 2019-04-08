4c5c06fc0d8080b3eabb39f41978196daf15a4ca
Shantnu Garg <gargshantnu@gmail.com>
2019-04-08 23:19:00 +0530

    basic website setup

--- training/views.py
+++ training/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2>Hey there</h2>')
+    # return HttpResponse('<h2>Hey there</h2>')
+    return render(request, "training/temp/TEST.HTML")
+
+def search(request):
+    return HttpResponse('<h2>Search Page</h2>')
